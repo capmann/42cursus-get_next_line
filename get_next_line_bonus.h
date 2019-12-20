0c8eab6f99ea878ac52b7b90a89c6810a9c93bfa
Capucine MARTEAU <cmarteau@e1r11p4.42.fr>
2019-12-20 20:05:10 +0100

    final

---
+++ get_next_line_bonus.h
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: cmarteau <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/12/20 19:58:42 by cmarteau          #+#    #+#             */
+/*   Updated: 2019/12/20 20:00:13 by cmarteau         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
+
+# include <sys/types.h>
+# include <sys/stat.h>
+# include <fcntl.h>
+# include <unistd.h>
+# include <stdio.h>
+# include <stdlib.h>
+
+int		get_next_line(int fd, char **line);
+char	*ft_strjoin(char *s1, char *s2);
+int		ft_search(char *s);
+size_t	ft_strlcat(char *dst, const char *src, size_t dstsize);
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
+char	*ft_substr(char *s, unsigned int start, size_t len);
+size_t	ft_strlen(const char *s);
+
+#endif
--- test.c
+++
@@ -1,4 +0,0 @@
-Hello world ! (:)
-Hola todos !
-Bonjour monde ! (:)
-bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb;
